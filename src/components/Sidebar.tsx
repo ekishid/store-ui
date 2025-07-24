74dea63704322a5d6bf9fe036b39e3cc603bac89
ekishid <ekishidev@gmail.com>
2025-07-24 00:49:55 -0600

    updated Header styles

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -9,8 +9,16 @@ interface SidebarProps {
   direction?: Direction
   children?: ReactNode
   className?: string
+  slideFromHeader?: boolean
 }
-const Sidebar = ({ isOpen, close, direction = 'ltr', children, className = '' }: SidebarProps) => {
+const Sidebar = ({
+  isOpen,
+  close,
+  direction = 'ltr',
+  children,
+  className = '',
+  slideFromHeader = false,
+}: SidebarProps) => {
   const sidebarRef = useClickOutside<HTMLDivElement>(() => {
     close()
   })
@@ -21,13 +29,16 @@ const Sidebar = ({ isOpen, close, direction = 'ltr', children, className = '' }:
     } else if (direction === 'rtl') {
       return isOpen ? 'right-0 h-full' : 'right-0 translate-x-[350px] h-full'
     } else if (direction === 'ttb') {
+      if (slideFromHeader) {
+        return isOpen ? 'top-[64px] left-0 w-full h-[300px]' : 'top-[64px] left-0 -translate-y-[364px] w-full h-[300px]'
+      }
       return isOpen ? 'top-0 left-0 w-full h-[300px]' : 'top-0 left-0 -translate-y-[300px] w-full h-[300px]'
     } else if (direction === 'btt') {
       return isOpen ? 'bottom-0 left-0 w-full h-[300px]' : 'bottom-0 left-0 translate-y-[300px] w-full h-[300px]'
     }
   }
   return (
-    <div ref={sidebarRef} className={twMerge(`z-50 bg-white w-[350px] fixed border-neutral-100 border shadow-lg transition-all ease-in-out duration-700 ${directionClass()} ${className}`)}>
+    <div ref={sidebarRef} className={twMerge(`z-50 w-[350px] fixed border-neutral-100 border shadow-lg transition-all ease-in-out duration-700 ${directionClass()} ${className}`)}>
       {children}
     </div>
   )
