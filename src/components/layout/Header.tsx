621e36db92eaa606f689829deb4e41ce1fcf1f0d
ekishid <ekishidev@gmail.com>
2025-07-24 00:49:34 -0600

    moved cart sidebar, updated styles

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,22 +3,24 @@ import {faBars, faCartShopping, faLayerGroup, faSearch, faUser} from '@fortaweso
 import {useDetectScroll} from '../../hooks/useDetectScroll.tsx'
 import {useState} from 'react'
 import {Link} from '@tanstack/react-router';
-import Sidebar from '../Sidebar.tsx';
 
 interface HeaderProps {
   showLogin: () => void
   showSidebar: () => void
+  showCart: () => void
+  isCartOpen: boolean
 }
 const Header = ({
   showLogin,
   showSidebar,
+  showCart,
+  isCartOpen,
 }: HeaderProps) => {
   const [hasDetectedScroll, headerRef] = useDetectScroll<HTMLDivElement>({})
   const [searching, setShowSearching] = useState<boolean>(false)
-  const [showCart, setShowCart] = useState<boolean>(false)
   return (
     <>
-      <header className={`z-40 w-full ${hasDetectedScroll ? 'h-[32px] fixed top-0 backdrop-blur-xs backdrop-saturate-150 shadow-neutral-300 shadow-lg border border-neutral-50 bg-white/30' : ''} min-h-[64px] px-6 flex flex-row items-center justify-between transition-all ease-in-out duration-300`}>
+      <header className={`z-40 w-full ${hasDetectedScroll ? `h-[32px] fixed top-0 backdrop-blur-xs backdrop-saturate-150 shadow-neutral-300 ${isCartOpen ? '' : 'shadow-lg border border-neutral-50'} bg-white/40` : `${isCartOpen ? 'backdrop-blur-xs backdrop-saturate-150 shadow-neutral-300 bg-white/40' : ''}`} min-h-[64px] px-6 flex flex-row items-center justify-between transition-all ease-in-out duration-300`}>
         <div className="flex flex-row items-center gap-3">
           <FontAwesomeIcon icon={faBars} className="cursor-pointer hover:scale-115 ease-in-out duration-300 transition-all" onClick={() => showSidebar()} />
           <div>Logo</div>
@@ -37,7 +39,7 @@ const Header = ({
               Categories
             </Link>
           </li>
-          <li className="cursor-pointer flex items-center gap-1 group" popoverTarget="cart" aria-label="Cart link" onClick={() => setShowCart(true)}>
+          <li className="cursor-pointer flex items-center gap-1 group" popoverTarget="cart" aria-label="Cart link" onClick={() => showCart()}>
             <FontAwesomeIcon icon={faCartShopping} className="group-hover:scale-120 duration-300 ease-in-out transition-all group-hover:animate-wiggle" />
             Cart
           </li>
@@ -46,13 +48,6 @@ const Header = ({
             Log in
           </li>
         </ul>
-        <Sidebar direction="ttb" isOpen={showCart} close={() => setShowCart(false)} className={`max-md:h-full z-50 bg-neutral-200 border-neutral-300 md:rounded-b-4xl ${showCart ? 'drop-shadow-2xl' : ''}`}>
-          <div className="px-3 pb-3 w-full h-full flex flex-col gap-4">
-            <div className="w-full h-full rounded-b-3xl p-3">
-              <button className="max-md:w-full bg-indigo-900 text-white border-0 hover:scale-[102%] duration-300 ease-in-out transition-all gap-2 flex items-center justify-center">Pay now</button>
-            </div>
-          </div>
-        </Sidebar>
       </header>
       <div ref={headerRef} />
     </>
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -15,15 +15,23 @@ export const Route = createRootRoute({
 function RootComponent() {
   const [showLogin, setShowLogin] = useState<boolean>(false)
   const [showSidebar, setShowSidebar] = useState<boolean>(false)
+  const [showCart, setShowCart] = useState<boolean>(false)
   return (
     <React.Fragment>
       <div className="h-screen w-screen flex flex-col">
-        <Header showLogin={() => setShowLogin(true)} showSidebar={() => setShowSidebar(true)} />
+        <Header isCartOpen={showCart} showCart={() => setShowCart(true)} showLogin={() => setShowLogin(true)} showSidebar={() => setShowSidebar(true)} />
         <Outlet />
         <Modal isOpen={showLogin} close={() => setShowLogin(false)}/>
         <Sidebar close={() => setShowSidebar(false)} isOpen={showSidebar} className="p-4 bg-neutral-200 border-neutral-300 shadow-lg">
           <div className="flex justify-end">
             <FontAwesomeIcon icon={faXmark} onClick={() => setShowSidebar(false)} className="cursor-pointer text-xl" />
+          </div>
+        </Sidebar>
+        <Sidebar slideFromHeader direction="ttb" isOpen={showCart} close={() => setShowCart(false)} className={`max-md:h-full z-20 backdrop-blur-xs backdrop-saturate-150 border-neutral-300 md:rounded-b-4xl ${showCart ? 'drop-shadow-2xl' : ''}`}>
+          <div className="px-3 pb-3 w-full h-full flex flex-col gap-4">
+            <div className="w-full h-full rounded-b-3xl p-3">
+              <button className="max-md:w-full bg-indigo-900 text-white border-0 hover:scale-[102%] duration-300 ease-in-out transition-all gap-2 flex items-center justify-center">Pay now</button>
+            </div>
           </div>
         </Sidebar>
         <Footer/>
